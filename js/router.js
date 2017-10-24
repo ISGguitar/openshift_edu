77ef2454a7710aa4e5e491ba1f1f7a46d7cdc328
Sergei Ikonnikov <isg.guitar@gmail.com>
2017-10-24 15:29:55 +0700

    debug

--- js/router.js
+++ js/router.js
@@ -1,8 +1,10 @@
 function route(urlRequested, handle, res) {
 	if (handle[urlRequested] === 'function') {
+		console.log('function found');
 		handle[urlRequested](res);
 		//..
 	} else {
+		console.warn('page not found')
 		handle['/pagenotfound'](res);
 		//..
 	}
