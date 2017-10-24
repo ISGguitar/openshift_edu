4b69b2685a398768e3e9aac2c4a97178b8b4dd20
Sergei Ikonnikov <isg.guitar@gmail.com>
2017-10-24 15:33:49 +0700

    debug

--- app.js
+++ app.js
@@ -7,11 +7,11 @@ const port = 8080;
 
 let handle = {};
 // pages here
-handle['/'] = handler.indexPage;
-handle['/index'] = handler.indexPage;
+handle[''] = handler.indexPage;
+handle['index'] = handler.indexPage;
 
-handle['/pagenotfound'] = handler.notFoundPage;
-handle['/about'] = handler.aboutPage;
-handle['/blog'] = handler.blogPage;
+handle['pagenotfound'] = handler.notFoundPage;
+handle['about'] = handler.aboutPage;
+handle['blog'] = handler.blogPage;
 
 server.startServer(hostname, port, router.route, handle);
--- js/server.js
+++ js/server.js
@@ -5,6 +5,7 @@ function startServer(hostname, port, route, handle) {
 	http.createServer((req, res) => {		
 		
 		let urlRequested = url.parse(req.url).pathname;
+		console.log(`urlRequested:${urlRequested}`);
 		route(urlRequested, handle, res);		
 	}).listen(port, hostname, function() {
 		console.log('Server is running');
