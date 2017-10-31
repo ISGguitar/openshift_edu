d19b51ea6e5185b4e39c49e7c64409b42ee7f77b
Sergei Ikonnikov <isg.guitar@gmail.com>
2017-10-31 18:46:08 +0700

    moving to streams

--- js/getDataFromFile.js
+++ js/getDataFromFile.js
@@ -1,40 +1,57 @@
 const fs = require('fs');
 
+function getDataFromFile(dataPath, contentType, res) {
+
+	var data = "";
+	const readStream = fs.createReadStream(dataPath).setEncoding('utf8');
+	//if fs.exists()
+	//if file not found - page not found
+	//res.statusCode = 404;
+ 	//res.setHeader('Content-Type', 'text/plain');
+	//res.end(`Error\n${err}`);
+	//..
+
+
+	//read stream event:'data'
+	readStream.on('data', (chunk) => {
+		console.info('stream is reading');
+		data += chunk;
+	});
+
+	//no data will be consumed event:'end'
+	readStream.on('end', () => {
+		console.info('end of file');
+		res.statusCode = 200;
+		res.setHeader('Content-Type'. contentType)		
+		res.end(data);
+	});
+
+	//close stream event:'close'
+	readStream.on('close', () => {
+		console.info('stream is closed');
+		
+	});
+
+	//error  event:'error'
+	readStream.on('error', (err) => {
+		console.error(`Some error:\n${err}`);
+		res.statusCode = 500;
+		res.setHeader('Content-Type'. 'text/html')
+		res.write('Internal Server Error')
+		res.end(err);
+	});
+}
 
 function getHtml(fileName, res){
-
 	let path=`./src/html/${fileName}.html`;
-	
-	let dataFromFile = fs.readFile(path, 'UTF-8', function(err, data) {
-		if (err) {
-			res.statusCode = 404;
- 			res.setHeader('Content-Type', 'text/plain');
-			res.end(`Error\n${err}`);			
-		} else {		
-			res.statusCode = 200;
- 			res.setHeader('Content-Type', 'text/html');
-			res.end(data);			
-		}		
-	});
+	getDataFromFile(path, 'text/html', res);
 }
 
 //get css too
 
  function getCssFromFile(res) {
- 	fs.readFile('./src/css/style1.css', function (err, data) {
- 		console.log('css');
-    	if (err) {
-    		res.statusCode = 404;
- 			res.setHeader('Content-Type', 'text/plain');
-			res.end(`Error\n${err}`);
-    	} else {    		
-    		res.statusCode = 200;
- 			res.setHeader('Content-Type', 'text/css');
-			res.end(data);	
-    	}      
-    	
-  	});
- }
+ 	getDataFromFile('./src/css/style1.css', 'text/css', res);	
+}
 
 module.exports.getHtml = getHtml;
 module.exports.getCssFromFile = getCssFromFile;
